09217cd451f2042c62cb48763199838bfe12a948
gayathribuddhika <39160904+gayathribuddhika@users.noreply.github.com>
2018-11-12 08:41:46 +0530

    fifth program
    nestedif

---
+++ Nestedif.java
@@ -0,0 +1,16 @@
+
+public class Nestedif {
+
+    
+    public static void main(String[] args) {
+        int x=2;
+        if(x==1){
+            System.out.println("one");
+        }else if(x==2){
+            System.out.println("two");
+        }else{ 
+            System.out.println("three");
+    }
+    
+}
+}
